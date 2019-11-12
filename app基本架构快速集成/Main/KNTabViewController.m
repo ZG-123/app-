e0c040d1ab2ab4b358e8fa11f813b2fc989f1997
ZG-123 <1002344082@qq.com>
2019-11-12 11:15:07 +0800

    代码调整
    代码

--- app基本架构快速集成/Main/KNTabViewController.m
+++ app基本架构快速集成/Main/KNTabViewController.m
@@ -65,6 +65,7 @@
     // 先给外面传进来的小控制器 包装 一个导航控制器
     KNNavViewController *nav = [[KNNavViewController alloc] initWithRootViewController:childVc];
     nav.navigationBar.barTintColor = [UIColor whiteColor];
+    nav.title = title;
     // 添加为子控制器
     [self addChildViewController:nav];
 }
